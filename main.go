53c0f747814df0ad9ee26f2851c19a0817f8db0e
acnologia000 <hridyanshbluedevil@hotmail.com>
2022-06-15 13:16:50 -0400

    c3

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"database/sql"
 	"log"
 	"net/http"
+	"time"
 )
 
 const DEBUG = true
@@ -11,17 +11,13 @@ const DEBUG = true
 func main() {
 
 	db, err := connect()
+	sessionTicker := time.NewTicker(SESSION_DURATION)
+	expireSession(sessionTicker)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	http.HandleFunc("/employerLogin", employerLogin(db))
+	http.HandleFunc("/employerLogin", login(db))
 	log.Fatal(http.ListenAndServe("0.0.0.0:5000", http.DefaultServeMux))
-}
-
-func employerLogin(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-
-	}
 }
